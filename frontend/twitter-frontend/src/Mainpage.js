be9b069c3ee29547ff37fa55241244259439b25e
Jinda Huang <jindahuang@Jindas-MacBook-Pro.local>
2020-05-08 13:31:54 -0400

    mainpage layout

--- frontend/twitter-frontend/src/Mainpage.js
+++ frontend/twitter-frontend/src/Mainpage.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Mainpage.css';
-import { Button, Nav, Navbar, Form, FormControl} from 'react-bootstrap'
+import { Button, Nav, Navbar, Form, FormControl, Badge} from 'react-bootstrap'
 import Card from 'react-bootstrap/Card';
 import axios from 'axios'
 
@@ -48,41 +48,40 @@ class Mainpage extends React.Component {
                     </Nav.Link>
                 </Navbar>
                 <span className = "main-body">
-                    <div className="tweet_container">
-                        <div id="profileStats">
-                            <ul>
-                                <li>Tweets 
-                                    <span className="profileStatsNumber">{this.state.num_tweets}</span>
-                                </li>
-                            </ul>
-                        </div>
                         <div id="profileInfo">
-                        <Card style={{ width: '18rem' }}>
+                            <Card
+                            bg={"Dark".toLowerCase()}
+                            text={'white'}
+                            style={{ width: '18rem' }}
+                            >
+                            <Card.Header>Profile</Card.Header>
                             <Card.Body>
-                                <Card.Title>{this.state.user_name}</Card.Title>
+                                <Card.Title>{this.props.cookies.cookies.cur_user} </Card.Title>
                                 <Card.Text>
                                 Ecopia Full Stack Intern
                                 </Card.Text>
                             </Card.Body>
-                        </Card>
+                            </Card>
                         </div>
-                    </div>
                 </span>
+                
                 <span className="main-body">
-                    <div className="tweet_container">
+                    <div id="tweet_container">
                         <div id='timeline'>
  		                    <div className='timelineHeader'>
-                                <ul>
-                                    <li><span className="badge badge-secondary">Tweets</span></li>
-                                </ul>
+                                <h2>
+                                    Tweets   <Badge variant="secondary">{this.state.num_tweets}</Badge>
+                                </h2>
                             </div>
                             {
                                 this.state.tweets.map(item => (
-                                    <div className="tweetContent"> 
-                                        <h3>{item.title}</h3>
-                                        <p>
-                                            {item.content}
-                                        </p>
+                                    <div class='tweet'>
+                                        <div className="tweetContent"> 
+                                            <h3>{item.title}</h3>
+                                            <p>
+                                                {item.content}
+                                            </p>
+                                        </div>
                                     </div>
                                 ))
                             }
