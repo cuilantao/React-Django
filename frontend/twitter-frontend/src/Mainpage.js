5297cbb00aa7ba37e5500aae1bf0605d087b8727
tony <clt2738461982@gmail.com>
2020-05-08 11:49:37 -0400

    fetch all tweets

--- frontend/twitter-frontend/src/Mainpage.js
+++ frontend/twitter-frontend/src/Mainpage.js
@@ -5,18 +5,33 @@ import { Button, Navbar, Form, FormControl} from 'react-bootstrap'
 import { Link } from 'react-router-dom';
 import Card from 'react-bootstrap/Card';
 import Nav from 'react-bootstrap/Nav';
+import axios from 'axios'
 
 class Mainpage extends React.Component {
     constructor(props){
         super(props)
-        console.log(this.props.cookies.cookies)
-        this.state = { use_name: "" };
+        this.state = {
+            tweets: [],
+            num_tweets:0
+        };
     }
 
     componentWillMount() {
+        let url = new URL("http://localhost:8000/tweets/all_tweets");
         console.log(this.props.cookies.cookies.cur_user)
-        this.setState({user_name: this.props.cookies.cookies.cur_user})
-      }
+        url.searchParams.set('user_name', this.props.cookies.cookies.cur_user);
+
+        axios.get(url).then(res => {
+            this.setState(
+                {
+                    tweets: res.data,
+                    num_tweets: res.data.length
+                }
+            )
+        }).catch(err => {
+            console.log(err)
+        })
+    }
 
     render(){
         return(
@@ -34,55 +49,35 @@ class Mainpage extends React.Component {
                         <Button variant="warning">Sign out</Button>
                     </Nav.Link>
                 </Navbar>
-                <span className="main-body">
+                <span className = "main-body">
                     <div className="tweet_container">
                         <div id="profileStats">
                             <ul>
                                 <li>Tweets 
-                                    <span className="profileStatsNumber">1</span>
+                                    <span className="profileStatsNumber">{this.state.num_tweets}</span>
                                 </li>
                             </ul>
                         </div>
-                        <div id="profileInfo">
-                        <Card style={{ width: '18rem' }}>
-                            <Card.Img variant="top" src="..." />
-                            <Card.Body>
-                                <Card.Title>{this.state.user_name}</Card.Title>
-                                <Card.Text>
-                                Ecopia Full Stack Intern
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                        </div>
+                    </div>
+                </span>
+                <span className="main-body">
+                    <div className="tweet_container">
                         <div id='timeline'>
-
  		                    <div className='timelineHeader'>
                                 <ul>
                                     <li><span className="badge badge-secondary">Tweets</span></li>
                                 </ul>
                             </div>
-                            <div className='tweet'>
-                                <div className="tweetIconContainer">
-                                </div>
-
-                                <div className="tweetContent"> 
-                                    <h3>{this.state.user_name} <span className="grey">@UofT</span></h3>
-                                    <p>
-                                        Another great day at UofT! <span className="hashtag">#UofT #GreatDay</span>
-                                    </p>
-                                </div>
-                            </div>
-                            <div className='tweet'>
-                                <div className="tweetIconContainer">
-                                </div>
-                        
-                                <div className="tweetContent"> 
-                                    <h3>{this.state.user_name} <span className="grey">@UofT</span></h3>
-                                    <p>
-                                        Check out some books at <span className="hashtag">#robarts</span>
-                                    </p>
-                                </div>
-                            </div>
+                            {
+                                this.state.tweets.map(item => (
+                                    <div className="tweetContent"> 
+                                        <h3>{item.title}</h3>
+                                        <p>
+                                            {item.content}
+                                        </p>
+                                    </div>
+                                ))
+                            }
                         </div>
                     </div>
                 </span>
